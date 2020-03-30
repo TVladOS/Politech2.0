ad535f472a0ae94bf9b2f7e591f2a646d138348f
Vladislav Taranenko <vtaranenko4@gmail.com>
2020-03-30 17:32:39 +0300

    Попытка №1

---
+++ Лабораторная 3 С++ (Высокий уровень)/Лабораторная 3 С++ (Высокий уровень).cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    string str;
+    cout << "Enter string: ";
+    getline(cin, str);
+    reverse(str.begin(), str.end());
+    cout << str << endl;
+    system("pause");
+    return 0;
+    setlocale(0, " ");
+}
