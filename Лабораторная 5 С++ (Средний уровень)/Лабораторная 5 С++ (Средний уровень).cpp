48f8e382cf0a4996199d8fc9a67a3bffb9abdd27
Vladislav Taranenko <61828759+TVladOS@users.noreply.github.com>
2020-04-27 18:17:26 +0300

    19 вариант. Средний уровень

---
+++ Лабораторная 5 С++ (Средний уровень)/Лабораторная 5 С++ (Средний уровень).cpp
@@ -0,0 +1,17 @@
+﻿#include <fstream>
+#include <iterator>
+using namespace std;
+
+int main() 
+//19 вариант. Записать в файл g все чётные числа файла f, а в файл h все нечётные
+{
+    ifstream ifs("f.txt");
+    ofstream ofs1("g.txt");
+    ofstream ofs2("h.txt");
+
+    for (istream_iterator<int> it = istream_iterator<int>(ifs); it != istream_iterator<int>(); ++it)
+        if (*it % 2) ofs2 << *it << ' ';
+        else ofs1 << *it << ' ';
+
+    return 0;
+}
